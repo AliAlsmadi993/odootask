6bde718e90c74b05745ba2573f78e8b4688bb74a
ali <alsmadiali993@gmail.com>
2025-11-02 13:37:13 +0000

    add estate account

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Estate Account",
+    "version": "1.0",
+    "depends": ["estate", "account"],
+    "author": "Ali Alsmadi",
+    "category": "Real Estate",
+    "description": "Link module between Real Estate and Accounting",
+    "application": False,
+    "installable": True,
+}
